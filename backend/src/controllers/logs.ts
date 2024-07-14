9cbc552779033dd5f41f99211396180a19bc4816
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-07-14 17:49:48 +0200

    fix: Serialize access to pod logs (#53)

--- backend/src/controllers/logs.ts
+++ backend/src/controllers/logs.ts
@@ -1,11 +1,15 @@
 import { KubernetesApi } from '../kubernetes/api.js'
 import { PodController } from './pod.js'
 import { WeakCache } from '../util/cache.js'
+import { Mutex } from '../util/mutex.js'
 
 export class LogsController {
   // The log strings may be large, so we use a weak cache to allow them to be garbage collected.
   // However, it's impossible to create a WeakRef to a string, so we wrap the string in an object.
   private readonly cache = new WeakCache<{ data: string }>(5000)
+
+  // Only allow fetching one log at a time.
+  private readonly mutex = new Mutex()
 
   constructor (
     private readonly k8s: KubernetesApi,
@@ -17,20 +21,26 @@ export class LogsController {
     namespace: string
     name: string
   }): Promise<string | undefined> {
-    // Input must relate to an existing pod
-    const podItem = await this.podController.findPod(pod)
-    if (podItem == null) {
-      return undefined
-    }
-    // Requesting logs for a pod that is starting is invalid and will result in a 400 error.
-    if (podItem.status?.phase === 'Pending' || podItem.status?.phase === 'Unknown') {
-      return undefined
-    }
-    const result = await this.cache.lazyCompute([pod.namespace, pod.name], async () => {
-      return {
-        data: await this.k8s.getPodLogs(pod)
+    let unlock
+    try {
+      unlock = await this.mutex.lock()
+      // Input must relate to an existing pod
+      const podItem = await this.podController.findPod(pod)
+      if (podItem == null) {
+        return undefined
       }
-    })
-    return result?.data
+      // Requesting logs for a pod that is starting is invalid and will result in a 400 error.
+      if (podItem.status?.phase === 'Pending' || podItem.status?.phase === 'Unknown') {
+        return undefined
+      }
+      const result = await this.cache.lazyCompute([pod.namespace, pod.name], async () => {
+        return {
+          data: await this.k8s.getPodLogs(pod)
+        }
+      })
+      return result?.data
+    } finally {
+      unlock?.()
+    }
   }
 }
