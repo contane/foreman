0a4fd85f562c316fa479d414d7fb76c0cd247e0d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-03-07 14:10:08 +0000

    fix: Migrate tailwind config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,17 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
 // @ts-expect-error - no typings for vite-plugin-favicons-inject
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     vitePluginFaviconsInject('public/favicon.svg', {
       appName: 'Foreman',
       appShortName: 'Foreman',
       appDescription: 'Foreman - A Renovate web UI'
     })
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-        autoprefixer()
-      ]
-    }
-  }
+  ]
 })
