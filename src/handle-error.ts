bc759881eb79915fd5143760a3df0aa20f191b76
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2026-03-07 09:45:13 +0000

    fix(backend): Handle `unknown` error type

--- src/handle-error.ts
+++ src/handle-error.ts
@@ -1,5 +1,5 @@
-import type { FastifyError, FastifyReply } from 'fastify'
 import { badRequest, internalServerError, payloadTooLarge, unsupportedMediaType } from 'backend'
+import type { FastifyReply } from 'fastify'
 
 /**
  * The error handling function that is used. This should be bound before registering any routes to handle their errors
@@ -8,14 +8,18 @@ import { badRequest, internalServerError, payloadTooLarge, unsupportedMediaType 
  * @param error The error that occurred.
  * @param reply The response object.
  */
-export async function handleError (error: FastifyError, reply: FastifyReply): Promise<void> {
+export async function handleError (error: unknown, reply: FastifyReply): Promise<void> {
   // JSON input error (thrown during parsing of the request body)
-  if (error instanceof SyntaxError && error.statusCode != null && error.statusCode < 500) {
+  if (error instanceof SyntaxError && 'statusCode' in error && typeof error.statusCode === 'number' && error.statusCode < 500) {
     return await badRequest(reply)
   }
 
   // Fastify content type parser errors
-  switch (error.code) {
+  const code = error instanceof Error && 'code' in error && typeof error.code === 'string'
+    ? error.code
+    : undefined
+
+  switch (code) {
     case 'FST_ERR_CTP_BODY_TOO_LARGE':
       return await payloadTooLarge(reply)
     case 'FST_ERR_CTP_INVALID_TYPE':
